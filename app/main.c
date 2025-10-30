cbb4c57038bd74f572c77659e7a24938f4ff3f5b
Justin White <jw@justinwhite.net>
2025-10-30 15:17:01 -0400

    up/down button options

--- app/main.c
+++ app/main.c
@@ -972,10 +972,18 @@ void MAIN_ProcessKeys(KEY_Code_t Key, bool bKeyPressed, bool bKeyHeld)
             MAIN_Key_MENU(bKeyPressed, bKeyHeld);
             break;
         case KEY_UP:
-            MAIN_Key_UP_DOWN(bKeyPressed, bKeyHeld, 1);
+            #ifdef ENABLE_FEAT_JW_MAIN_UPDN_REV
+                MAIN_Key_UP_DOWN(bKeyPressed, bKeyHeld, -1);
+            #else
+                MAIN_Key_UP_DOWN(bKeyPressed, bKeyHeld, 1);
+            #endif
             break;
         case KEY_DOWN:
-            MAIN_Key_UP_DOWN(bKeyPressed, bKeyHeld, -1);
+            #ifdef ENABLE_FEAT_JW_MAIN_UPDN_REV
+                MAIN_Key_UP_DOWN(bKeyPressed, bKeyHeld, 1);
+            #else
+                MAIN_Key_UP_DOWN(bKeyPressed, bKeyHeld, -1);
+            #endif
             break;
         case KEY_EXIT:
             MAIN_Key_EXIT(bKeyPressed, bKeyHeld);
